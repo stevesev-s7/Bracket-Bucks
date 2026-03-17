57aeb9983f2be5e59447e632472987e2b0c35cd4
stevesev-s7 <stephen.sevenich@gmail.com>
2026-03-17 10:39:35 -0500

    Bump: force Vercel to build latest clean code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// v1773758858652
+// v1773761975366
 import React, { useState, useEffect, useCallback } from "react";
 import { supabase } from "./supabaseClient";
 const _APP_BUILD = "1773204216116";
