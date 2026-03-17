d6fa07db68bf0c11943dffea7cbc032ba2375f92
stevesev-s7 <stephen.sevenich@gmail.com>
2026-03-16 23:58:00 -0500

    Force rebuild: How to Play button + modal in main app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// v1773286522751
+// v1773723480079
 import React, { useState, useEffect, useCallback } from "react";
 import { supabase } from "./supabaseClient";
 const _APP_BUILD = "1773204216116";
